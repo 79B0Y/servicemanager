4dd6f62f66f62507e6a63d5941caf860ad0c8cf8
79B0Y <jackyao791104@gmail.com>
2025-08-03 12:58:11 +0800

    Update install.sh

--- matter-server/install.sh
+++ matter-server/install.sh
@@ -273,7 +273,7 @@ cat << 'EOF' > "$RUN_FILE"
 exec proot-distro login "$PROOT_DISTRO" -- bash -c '
     cd "$MATTER_INSTALL_DIR"
     source "$MATTER_ENV_DIR/bin/activate"
-    exec matter_server
+    exec matter-server
 '
 EOF
 
