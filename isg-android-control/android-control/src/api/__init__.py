bfae600b786f761895dca6acaa70931d8ebf1345
79B0Y <jackyao791104@gmail.com>
2025-08-27 12:50:57 +0800

    Delete isg-android-control/android-control directory

--- isg-android-control/android-control/src/api/__init__.py
+++
@@ -1,4 +0,0 @@
-from isg_android_control.api.main import app, create_app
-
-__all__ = ["app", "create_app"]
-
--- isg-android-control/android-control/src/services/__init__.py
+++
@@ -1,6 +0,0 @@
-from isg_android_control.services.screenshot import ScreenshotService
-from isg_android_control.services.monitor import MonitorService
-from isg_android_control.services.cache import Cache
-
-__all__ = ["ScreenshotService", "MonitorService", "Cache"]
-
